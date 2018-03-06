26c36473c97f8ac22d2784ebc3744b2f29c9a5e1
tinfante <correo@deverdad.cl>
2018-03-06 18:47:45 -0300

    Finished ex3 for ch4.

--- src/brave_clojure/ch4_ex.clj
+++ src/brave_clojure/ch4_ex.clj
@@ -5,7 +5,7 @@
 
 
 ;; 1. Turn the result of your glitter filter into a list of names.
-(def suspects 
+(def suspects
   (fwpd/glitter-filter 3 (fwpd/mapify (fwpd/parse (slurp fwpd/filename))))
   )
 
@@ -22,7 +22,7 @@
 
 ;; 2. Write a function, append, which will append a new suspect to your
 ;; list of suspects.
-(defn append
+(defn append-suspect
   [suspect-map-list suspect-name suspect-index]
   (conj suspect-map-list {:name suspect-name :glitter-index suspect-index})
   )
@@ -30,7 +30,7 @@
 (defn ex2
   []
   (println
-    (append suspects "Tomas Infante" -1)
+    (append-suspect suspects "Tomas Infante" -1)
     )
   )
 
@@ -39,13 +39,31 @@
 ;; :glitter-index are present when you append. The validate function
 ;; should accept two arguments: a map of keywords to validating
 ;; functions, similar to conversions, and the record to be validated.
-;; TODO
+(def validations
+  {:name (fn [x] (string? x))
+   :glitter-index (fn [x] (integer? x))
+   }
+  )
+
+(defn validate-key-value
+  [vamp-key value]
+  ((get validations vamp-key) value)
+  )
+
+(defn validate-all-key-values
+  [record]
+  (every?
+    (fn [k] (validate-key-value k (k record)))
+    (map (fn [[k v]] k) validations)
+    )
+  )
+
 (defn validate
   [validations record]
   (if (every? (fn [k] (contains? record k))
               (map (fn [[k v]] k) validations))
-    ; Record has all validation keys.
-    true
+    ; Record has all validation keys. Check if key values OK.
+    (validate-all-key-values record)
     ; Record doesn't have all validation keys.
     false
     )
@@ -53,14 +71,27 @@
 
 (defn ex3
   []
-  (println (validate {:name "a" :glitter-index 9}
-                     ;{:name "Mal Registro"}
-                     {:name "Yuri Guigan" :glitter-index 1000}
-             )
-    )
+  (println (str "{:name \"Tomas Infante\" :glitter-index 1} -> "
+                (validate validations {:name "a" :glitter-index 9})))
+  (println (str "{:name \"Tomas Infante\"} -> "
+                (validate validations {:name "Tomas Infante"})))
+  (println (str "{:name \"Tomas Infante\" :glitter-index \"one\"} -> "
+                (validate validations {:name "Tomas Infante" :glitter-index "uno"})))
   )
 
 
 ;; 4. Write a function that will take your list of maps and convert it
 ;; back to a CSV string. You’ll need to use the clojure.string/join function.
 ;; TODO
+(defn suspect-map-list->csv-string
+  [suspect-map-list]
+  (clojure.string/join
+    "\n"
+    (map (fn [sm] (str (:name sm) "," (:glitter-index sm))) suspect-map-list)
+    )
+  )
+
+(defn ex4
+  []
+  (println (suspect-map-list->csv-string suspects))
+  )
